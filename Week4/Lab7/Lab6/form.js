0676a8c8b134316076c666842f438bd9056cf741
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-02-19 16:18:04 +0000

    test

--- Week4/Lab7/Lab6/form.js
+++ Week4/Lab7/Lab6/form.js
@@ -1,25 +1,25 @@
 function main () {
-	var a b
+	var a, b
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
-	b = + a b;
-	c == a * b;
-	
+
+	b =  a + b;
+	c = a * b;
+
 	document.writeln(c);
 }
 
 // function main () {
 // 	var a, b;
 // 	var c;
-// 	
+//
 // 	a = 10;
 // 	b = 100;
-// 	
+//
 // 	b = a + b;
 // 	c = a * b;
-// 	
+//
 // 	document.writeln(c);
 // }

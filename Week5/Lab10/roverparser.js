438919cedcdf7a026a283ad0a88c08175cf10270
unknown <iivo.mitrunen@gmail.com>
2018-03-02 04:09:42 +0200

    test

--- Week5/Lab10/roverparser.js
+++ Week5/Lab10/roverparser.js
@@ -20,13 +20,14 @@
   console.log(result.photos);
   var photos = result.photos //this data contains an array called photos
 
+
   var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>earthDate</th><th>LaunchDate</th></thead><tbody>";
   for (var i in photos)
   {
     output+="<tr><td>"
     + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].<img src = "this.img_src"> + "</td><td>"
+    + photos[i].img_src = img src  + "</td><td>"
     + photos[i].earth_date + "</td><td>"
     + photos[i].rover.launch_date + "</td></tr>";
 

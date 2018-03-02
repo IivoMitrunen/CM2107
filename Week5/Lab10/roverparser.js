83c9e7f11da0d043d451851b3a90b2e2d5c32bfb
unknown <iivo.mitrunen@gmail.com>
2018-03-02 03:23:01 +0200

    test

--- Week5/Lab10/roverparser.js
+++ Week5/Lab10/roverparser.js
@@ -24,9 +24,9 @@
   for (var i in photos)
   {
     output+="<tr><td>"
-     + photos[i].rover.name + "</td><td>"
+    + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].earth_date + "</td><td>"
+    + photos[i].earth_date + "</tr><td>"
     + photos[i].img_src + "</td></tr>";
 
   }

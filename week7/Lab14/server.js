e3ba9b54fc96e2e4be41e5d8fc4a36d332d44c12
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-03-15 09:56:28 +0000

    test

--- week7/Lab14/server.js
+++ week7/Lab14/server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
- app.use(express.static('public'))
- app.get('/'
+var Twitter = require('twitter');
+var client = new Twitter({
+ consumer_key: 'YrvgnucqTgZNvjVx6o9DJGuq1',
+ consumer_secret: 'DShKgNOjaWQg7QxZHgzt6fcz3Wk12LPIdEK5agjPV6shIoaozG',
+ access_token_key: '802550256902819840-qhXjmn3TH3pDzpsgIWcBCIbQ12oU5H9',
+ access_token_secret: 'dubHwb1UBn1g1NbXGrjfbuqEMTghmJn9LmUNgG62IcMLp'
+});
+app.use(express.static('public'))
+app.get('/'
 , function(req, res){
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (!error) {
+ console.log(tweets);
+ }
+});
+  var params = {screen_name: 'nodejs'};
  res.send("Hello world! by express");
 });
+
 app.listen(8080);

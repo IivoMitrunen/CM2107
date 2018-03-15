75d392aa25084b316db11af08a532bfa7f6b4c4f
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-03-15 10:29:15 +0000

    test

--- week7/Lab14/server.js
+++ week7/Lab14/server.js
@@ -13,7 +13,7 @@ app.get('/'
 , function(req, res){
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
  if (!error) {
- console.log(tweets);
+ res.send(tweets);
  }
 });
 

e6ac602f23fac737e04522761c91557e77cf4664
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-03-19 15:53:20 +0000

    test

--- week7/Lab13/mythirdserver.js
+++ week7/Lab13/mythirdserver.js
@@ -1,7 +1,7 @@
 
 
 var http = require('http');
-var oneLinerJoke = require('one-liner-joke');
+var oneLinerJoke = require('git puone-liner-joke');
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
 var randomJoke = oneLinerJoke.getRandomJoke();

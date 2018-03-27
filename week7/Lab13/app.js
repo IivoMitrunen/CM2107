b93d24d4e3165a9a8a73e3dff5ece4c4f278b092
unknown <iivo.mitrunen@gmail.com>
2018-03-27 14:16:35 +0100

    test

--- week7/Lab13/app.js
+++ week7/Lab13/app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res){
- res.send("Hello world! by express");
+app.get('/', function(req, res) {
+  res.send("Hello world! by express");
 });
 app.listen(8080);
 
-app.get('/test', function(req, res){
- res.send("this is route 2");
+app.get('/test', function(req, res) {
+  res.send("this is route 2");
 });
 
 
-app.get('/add', function(req, res){
-var x = parseInt(req.query.x);
-var y = parseInt(req.query.y);
- res.send("X + Y="+(x+y));
+app.get('/add', function(req, res) {
+  var x = req.query.x;
+  var y = req.query.y;
+  res.send("X + Y=" + (x + y));
 });
